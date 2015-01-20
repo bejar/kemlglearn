b581e878289aff4da2e792222debf89cebcd639f
bejar <bejar@lsi.upc.edu>
2015-01-20 15:22:56 +0100

    Implementation of the global k means

---
+++ kemlglearn/cluster/GlobalKMeans.py
@@ -0,0 +1,187 @@
+"""
+.. module:: GlobalKMeans
+
+GlobalKMeans
+*************
+
+:Description: GlobalKMeans
+
+    
+
+:Authors: bejar
+    
+
+:Version: 
+
+:Created on: 20/01/2015 10:42 
+
+"""
+
+__author__ = 'bejar'
+
+
+import numpy as np
+from sklearn.base import BaseEstimator, ClusterMixin, TransformerMixin
+from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.cluster import KMeans
+from sklearn.neighbors import NearestNeighbors
+
+
+class GlobalKMeans(BaseEstimator,ClusterMixin,TransformerMixin):
+    """Global K-means Algorithm
+
+    Paramereters:
+
+    n_clusters: int
+        maximum number of clusters to obtain
+
+    """
+    def __init__(self, n_clusters):
+        self.n_clusters = n_clusters
+        self.cluster_centers_ = None
+        self.labels_ = None
+        self.cluster_sizes_ = None
+        self.inertia_ = None
+
+    def fit(self,X):
+        """
+        Clusters the examples
+        :param X:
+        :return:
+        """
+
+        self.cluster_centers_, self.labels_, self.inertia_ = self._fit_process(X)
+
+        return self
+
+    def predict(self,X):
+        """
+        Returns the nearest cluster for a data matrix
+
+        @param X:
+        @return:
+        """
+        clasif = []
+        for i in range(X.shape[0]):
+            ncl, mdist = self._find_nearest_cluster(X[i], self.cluster_centers_)
+            if mdist <= self.radius:
+                clasif.append(ncl)
+            else:
+                clasif.append(-1)
+        return clasif
+
+    def _fit_process(self, X):
+        """
+        Clusters using the global K-means algorithm
+        :param X:
+        :return:
+        """
+
+        # Create a KNN structure for fast search
+        self._neighbors = NearestNeighbors()
+        self._neighbors.fit(X)
+
+        # Compute the centroid of the dataset
+        centroids = sum(X)/X.shape[0]
+        assignments = [0 for i in range(X.shape[0])]
+
+        centroids.shape = (1, X.shape[1])
+
+        # compute the distance of the examples to the centroids
+        mindist = np.zeros(X.shape[0])
+        for i in range(X.shape[0]):
+            mindist[i] = euclidean_distances(X[i], centroids[assignments[i]])[0]
+
+        for k in range(2, self.n_clusters+1):
+            newCentroid = self._compute_next_centroid(X, centroids, assignments, mindist)
+            centroids = np.vstack((centroids,newCentroid))
+            km = KMeans(n_clusters=k, init=centroids, n_init=1)
+            km.fit(X)
+            assignments = km.labels_
+            for i in range(X.shape[0]):
+                mindist[i] = euclidean_distances(X[i], centroids[assignments[i]])[0]
+
+        return km.cluster_centers_, km.labels_, km.inertia_
+
+    def _compute_next_centroid(self, X, centroids, assignments, mindist):
+        """
+        Computes the candidate for the next centroid
+
+        :param X:
+        :param centroids:
+        :return:
+        """
+        minsum = np.infty
+        candCentroid = None
+
+        # Compute the first candidate to new centroid
+        for i in range(X.shape[0]):
+            distance = euclidean_distances(X[i], centroids[assignments[i]])[0]
+            S2 = self._neighbors.radius_neighbors(X[i], radius=distance, return_distance=False)[0]
+            S2centroid = sum(X[S2])/len(S2)
+            S2centroid.shape = (1, X.shape[1])
+            cost = self._compute_fk(X, mindist, S2centroid)
+
+            if cost < minsum:
+                minsum = cost
+                candCentroid = S2centroid
+
+        # Compute examples for the new centroid
+        S2 = []
+        newDist = euclidean_distances(X, candCentroid)
+        for i in range(X.shape[0]):
+            if newDist[i] < mindist[i]:
+                S2.append(i)
+
+        newCentroid = sum(X[S2])/len(S2)
+        newCentroid.shape = (1, X.shape[1])
+
+        while not (candCentroid == newCentroid).all():
+            candCentroid = newCentroid
+            S2 = []
+            newDist = euclidean_distances(X, candCentroid)
+            for i in range(X.shape[0]):
+                if newDist[i] < mindist[i]:
+                    S2.append(i)
+
+            newCentroid = sum(X[S2])/len(S2)
+            newCentroid.shape = (1, X.shape[1])
+
+        return candCentroid
+
+
+    def _compute_fk(self, X, mindist, ccentroid):
+        """
+        Computes the cost function
+
+        :param X:
+        :param mindist:
+        :param ccentroid:
+        :return:
+        """
+
+        # Distances among the examples and the candidate centroid
+        centdist = euclidean_distances(X, ccentroid)
+
+        fk = 0
+        for i in range(X.shape[0]):
+            fk = fk + min(mindist[i], centdist[i][0])
+
+        return fk
+
+
+    @staticmethod
+    def _find_nearest_cluster(examp, centers):
+        """
+        Finds the nearest cluster for an example
+        :param examp:
+        :param centers:
+        :return:
+        """
+
+        dist = euclidean_distances(centers, examp)
+
+        pmin = np.argmin(dist)
+        vmin = np.min(dist)
+
+        return pmin, vmin
--- kemlglearn/cluster/test.py
+++ kemlglearn/cluster/test.py
@@ -16,31 +16,58 @@ test
 :Created on: 07/07/2014 11:12 
 
 """
-from Leader import Leader
-
 __author__ = 'bejar'
 
+
+
+from Leader import Leader
+from GlobalKMeans import GlobalKMeans
 from sklearn.datasets import make_blobs
 import pylab as pl
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 
+X, y_data = make_blobs(n_samples=1000, n_features=20, centers=7, random_state=2)
 
-X,y_data = make_blobs(n_samples=5200, n_features=20, centers=7, random_state=0)
+# ld = Leader(radius=25.0)
+#
+# #print timeit.timeit(stmt='ld.fit(X)',setup=setup,number=10)
+# ld.fit(X)
+#
+# # print ld.cluster_centers_.shape[0]
+#
+# fig = plt.figure()
+#
+# # ax = fig.gca(projection='3d')
+# # pl.scatter(X[:, 1], X[:, 2], zs=X[:, 0], c=ld.labels_, s=25)
+# #
+# ax = fig.add_subplot(111)
+# plt.scatter(X[:,0],X[:,1],c=ld.labels_)
+#
+# plt.show()
 
-ld = Leader(radius=25.0)
+gkm = GlobalKMeans(n_clusters=7)
+gkm.fit(X)
 
-#print timeit.timeit(stmt='ld.fit(X)',setup=setup,number=10)
-ld.fit(X)
-
-# print ld.cluster_centers_.shape[0]
+print gkm.cluster_centers_.shape[0]
 
 fig = plt.figure()
 
 # ax = fig.gca(projection='3d')
-# pl.scatter(X[:, 1], X[:, 2], zs=X[:, 0], c=ld.labels_, s=25)
+# pl.scatter(X[:, 1], X[:, 2], zs=X[:, 0], c=gkm.labels_, s=25)
 #
 ax = fig.add_subplot(111)
-plt.scatter(X[:,0],X[:,1],c=ld.labels_)
+plt.scatter(X[:,0],X[:,1],c=gkm.labels_)
 
 plt.show()
 
+km = KMeans(n_clusters=7)
+km.fit(X)
+print km.cluster_centers_.shape[0]
+
+fig = plt.figure()
+
+ax = fig.add_subplot(111)
+plt.scatter(X[:,0],X[:,1],c=km.labels_)
+
+plt.show()
